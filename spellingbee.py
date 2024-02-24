232ecffdb44749c9c8d351d5cebd765a2f0d3368
[Dillon Reading] <dillreading@gmail.com>
2024-02-24 15:12:30 -0700

    worked out some bugs in panagrams and print statements

--- spellingbee.py
+++ spellingbee.py
@@ -1,15 +1,16 @@
 from util import get_single_letter
 from localdict import LocalDict
+from time import sleep
 
 if __name__ == "__main__":
-    print("Welcome to the ", end="")
+    print("\nWelcome to the ", end="")
     print("\033[1;33;40mSpelling Bee Solver\033[0m!")
     print("Based on the NYT's hit game \033[1;33;40mSpelling Bee\033[0m\n")
 
-    print("This game takes one key letter and 6 other letters then generates words")
-    print("\nThese words must follow some rules:\n"
-          "\t1. It can only be created using the 7 input letters\n"
-          "\t2. it must contain the key letter\n"
+    print("This solver takes one key letter and 6 other letters then generates words from those letters.")
+    print("\nHowever, these words must follow some rules:\n"
+          "\t1. They can only be created using the 7 input letters\n"
+          "\t2. They must contain the key letter\n"
           "\t3. All words must be at least 4 letters long")
 
     input("\nHit Enter when you're ready to input your characters\n")
@@ -22,7 +23,8 @@ if __name__ == "__main__":
     for i in range(6):
         letters.append(get_single_letter())
 
-    print("Perfect! Please wait while we get all the possible words...\n")
+    print("\nPerfect! Please wait while we get all the possible words...\n")
+    sleep(2)
 
     d = LocalDict()
     answers = d.possible_words(letters, key)
@@ -30,23 +32,32 @@ if __name__ == "__main__":
     answers = list(set(answers))
     answers.sort()
 
-    print("Here is a list of possible answers:\n")
+    print("Here is a list of possible answers:")
     count = 0
-    for word in answers:
-        if count < 14:
-            print(word + ", ", end="")
-            count += 1
-        elif count == 14:
+    for i, word in enumerate(answers):
+        if i == len(answers) - 1:
             print(word)
-            count = 0
+        else:
+            if count < 14:
+                print(word + ", ", end="")
+                count += 1
+            elif count == 14:
+                print(word)
+                count = 0
 
-    print("\n\nBut lets be honest you aren't just looking for any normal answers...")
-    print("Hit Enter to see the possible \033[1;31;40mpangrams\033[0m")
+    print("\n\nBut lets be honest you aren't here looking for normal answers...")
+    print("Hit Enter to see today's possible \033[1;31;40mpangrams\033[0m")
     input()
     pangrams = d.possible_pangram(letters, answers)
 
-    print("Today's potential \033[1;31;40mpangrams\033[0m are:")
-    for pangram in pangrams:
-        print("\033[1;31;40m" + pangram + "\033[0m")
+    if len(pangrams) == 0:
+        print("Looks like our dictionary can't find any \033[1;31;40mpangrams\033[0m...\n"
+              "Are you sure you put in the right letters?")
+    else:
+        print("Today's potential \033[1;31;40mpangrams\033[0m are:")
+        for pangram in pangrams:
+            print("\033[1;31;40m" + pangram + "\033[0m")
 
-    print("\nThanks for using the \033[1;33;40mSpelling Bee Solver\033[0m!")
+    print("\n(Please note that our dictionary does not always match up with NYT's dictionary)")
+
+    print("\nThanks for using the \033[1;33;40mSpelling Bee Solver\033[0m!\n")
